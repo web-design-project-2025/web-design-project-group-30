d44067b5a89d07031a533a80a70297e6d5fc77f2
Saarahuss <saara.hussein5@gmail.com>
2025-04-16 13:42:49 +0200

    added the search bar aswell as filtered searches so that it is possible to search for the options existing

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// search bar
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("search-bar");
+  filter = input.value.toUpperCase();
+  ul = document.getElementsByClassName("search-result")[0];
+  li = ul.getElementsByTagName("li");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+const searchInput = document.getElementById("search-bar");
+const searchList = document.querySelector(".search-result");
+
+searchInput.addEventListener("focus", () => {
+  searchList.style.display = "block";
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !searchInput.contains(event.target) &&
+    !searchList.contains(event.target)
+  ) {
+    searchList.style.display = "none";
+  }
+});
