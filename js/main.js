254701dab7e66f5329bf973eba6775163b5525f2
KimberlyMichalak <Kimy.mck@gmail.com>
2025-05-02 21:57:33 +0200

    fetched data for the trending page and implemented client side logic and added images

--- js/main.js
+++ js/main.js
@@ -34,31 +34,7 @@ fetch("movie.json")
 //   }
 // }
 
-// // carousel effect
-// document.addEventListener("click", function (event) {
-//   const button = document.querySelector(".categories-row button");
-//   const list = document.querySelector(".category-list");
 
-//   if (!button.contains(event.target) && !list.contains(event.target)) {
-//     list.classList.remove("show");
-//   }
-// });
-
-// const productContainers = [...document.querySelectorAll(".product-container")];
-// const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
-// const preBtn = [...document.querySelectorAll(".pre-btn")];
-
-// productContainers.forEach((item, i) => {
-//   let containerDimensions = item.getBoundingClientRect();
-//   let containerWidth = containerDimensions.width;
-
-//   nxtBtn[i].addEventListener("click", () => {
-//     item.scrollLeft += containerWidth;
-//   });
-//   preBtn[i].addEventListener("click", () => {
-//     item.scrollLeft -= containerWidth;
-//   });
-// });
 
 // // search bar
 // function filterSearchResults() {
